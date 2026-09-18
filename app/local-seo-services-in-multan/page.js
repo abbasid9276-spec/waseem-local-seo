4d6b5217f943ccba0df7cb555d99a4aa5cdb8e5d
abbasid9276-spec <abbasid9276@gmail.com>
2026-09-18 16:40:44 +0500

    Create page.js

---
+++ app/local-seo-services-in-multan/page.js
@@ -0,0 +1,24 @@
+import CityPage, { citySchema } from "../../components/CityPage";
+import { cities } from "../../lib/cities";
+import { site } from "../../lib/site";
+
+const data = cities.multan;
+
+export const metadata = {
+  title: data.title,
+  description: data.metaDesc,
+  alternates: { canonical: `/${data.slug}` },
+  openGraph: { type: "website", url: `${site.url}/${data.slug}`, title: data.title, description: data.metaDesc },
+};
+
+export default function Page() {
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(citySchema(data, site.url)) }}
+      />
+      <CityPage data={data} />
+    </>
+  );
+}
